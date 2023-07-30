7bb00021d70e1a2553f9b9cebe41236fa02e4bf9
Hajime Yagihara <eitiwai@gmail.com>
2023-07-30 16:22:56 +0900

    Update merge_tokenizers.py
    change LLamaTokenizer to AutoTokenizer

--- scripts/merge_tokenizer/merge_tokenizers.py
+++ scripts/merge_tokenizer/merge_tokenizers.py
@@ -1,6 +1,6 @@
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"]="python"
-from transformers import LlamaTokenizer
+from transformers import AutoTokenizer
 from sentencepiece import sentencepiece_model_pb2 as sp_pb2_model
 import sentencepiece as spm
 import argparse
@@ -13,7 +13,7 @@ llama_tokenizer_dir = args.llama_tokenizer_dir
 japanese_sp_model_file = args.japanese_sp_model_file
 
 # load
-llama_tokenizer = LlamaTokenizer.from_pretrained(llama_tokenizer_dir)
+llama_tokenizer = AutoTokenizer.from_pretrained(llama_tokenizer_dir)
 japanese_sp_model = spm.SentencePieceProcessor()
 japanese_sp_model.Load(japanese_sp_model_file)
 
@@ -47,15 +47,15 @@ output_hf_dir = 'merged_tokenizer_hf' # the path to save japanese-LLaMA tokenize
 os.makedirs(output_sp_dir,exist_ok=True)
 with open(output_sp_dir+'/japanese_llama.model', 'wb') as f:
     f.write(llama_spm.SerializeToString())
-tokenizer = LlamaTokenizer(vocab_file=output_sp_dir+'/japanese_llama.model')
+tokenizer = AutoTokenizer(vocab_file=output_sp_dir+'/japanese_llama.model')
 
 tokenizer.save_pretrained(output_hf_dir)
 print(f"japanese-LLaMA tokenizer has been saved to {output_hf_dir}")
 
 
 # Test
-llama_tokenizer = LlamaTokenizer.from_pretrained(llama_tokenizer_dir)
-japanese_llama_tokenizer = LlamaTokenizer.from_pretrained(output_hf_dir)
+llama_tokenizer = AutoTokenizer.from_pretrained(llama_tokenizer_dir)
+japanese_llama_tokenizer = AutoTokenizer.from_pretrained(output_hf_dir)
 print(tokenizer.all_special_tokens)
 print(tokenizer.all_special_ids)
 print(tokenizer.special_tokens_map)
